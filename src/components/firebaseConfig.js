9f35bc5c3445335ad0c58e1b6c305a5a66d38dc6
Kirti Gupta <kirtiigupta12@gmail.com>
2023-02-01 15:56:58 +0530

    Add files via upload

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -6,13 +6,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAe-xUqd5kgOWkgSrBAu2JvWe6Aaiv7luc",
-  authDomain: "netflix-dc37e.firebaseapp.com",
-  projectId: "netflix-dc37e",
-  storageBucket: "netflix-dc37e.appspot.com",
-  messagingSenderId: "666793934171",
-  appId: "1:666793934171:web:5c02694b8f6fdddf096e7e",
-  measurementId: "G-98E5YSE95K"
+  apiKey: "AIzaSyCsJN8sVewrbBNq544s58NXlx_H8zk38F8",
+  authDomain: "netflix-3bc9b.firebaseapp.com",
+  projectId: "netflix-3bc9b",
+  storageBucket: "netflix-3bc9b.appspot.com",
+  messagingSenderId: "322117323063",
+  appId: "1:322117323063:web:2d42a4786e2eb9ae41f2b2",
+  measurementId: "G-RHK93D907W"
 };
 
 // Initialize Firebase
